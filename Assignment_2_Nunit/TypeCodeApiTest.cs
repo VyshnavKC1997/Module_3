ef69e53ba1999ff89805a9e60d2854c367274bb9
vyshnav.kunnumalchalil <vyshnav.kunnumalchalil@ust.com>
2023-12-12 16:27:59 +0530

    Assignment_2 Added

---
+++ Assignment_2_Nunit/TypeCodeApiTest.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Assignment_2_Nunit
+{
+    internal class TypeCodeApiTest
+    {
+        static string baseUrl = "https://jsonplaceholder.typicode.com/";
+        RestClient client = new RestClient(baseUrl);
+
+        [Test]
+        [TestCase("3")]
+        public void GetSingleTodoTest(string id)
+        {
+            var createToDoListRequest = new RestRequest("todos/" + id, Method.Get);
+            var response=client.Execute(createToDoListRequest);
+
+            Assert.That(response.StatusCode,Is.EqualTo(System.Net.HttpStatusCode.OK));
+
+        }
+
+        [Test]
+        public void GetAllTodoTest()
+        {
+            var createToDoListRequest = new RestRequest("todos", Method.Get);
+            var response= client.Execute(createToDoListRequest);
+            var userData=JsonConvert.DeserializeObject<List<UserData>>(response.Content);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            Assert.That(userData,Is.Not.Null);
+            Assert.That(userData[0].Title, Is.Not.Null);
+
+        }
+        [Test]
+        public void CreateTodoList()
+        {
+            var createToDoListRequest = new RestRequest("todos", Method.Post);
+            createToDoListRequest.AddHeader("Content-Type", "application/json");
+            createToDoListRequest.AddBody(new { userId = 145, title = "adhghgadgha", completed = true });
+            var response=client.Execute(createToDoListRequest);
+            var userData = JsonConvert.DeserializeObject<UserData>(response.Content);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+            Assert.That(userData, Is.Not.Null);
+            Assert.That(userData.Title, Is.Not.Null);
+        }
+        [Test]
+        [TestCase("3")]
+        public void DeleteTodoList(string id) 
+        {
+            var createToDoListRequest = new RestRequest("todos/" + id, Method.Delete);
+            var response = client.Execute(createToDoListRequest);
+
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+        }
+        [Test]
+        [TestCase("3")]
+        public void UpdateTodoList(string id)
+        {
+            var createToDoListRequest = new RestRequest("todos/" + id, Method.Put);
+            createToDoListRequest.AddBody(new { userId = 145, title = "adhghgadgha", completed = true });
+            var response=client.Execute(createToDoListRequest);
+            var userData = JsonConvert.DeserializeObject<UserData>(response.Content);
+            Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+            Assert.That(userData, Is.Not.Null);
+            Assert.That(userData.Title, Is.Not.Null);
+
+        }
+    }
+}
---
+++ Assignment_2_Nunit/UserData.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_Nunit
+{
+    public class UserData
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("userid")]
+        public string? UserId { get; set; }
+        [JsonProperty("title")]
+        public string? Title{ get; set; }
+        [JsonProperty("completed")]
+        public bool IsCompleted { get; set; }
+
+    }
+  
+}
